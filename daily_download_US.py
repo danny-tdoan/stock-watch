35a7b6457a6fd0a8546a6f5bf2410118bb370d6e
Danny <mni.empty@gmail.com>
2016-08-06 16:43:32 +1000

    update US download script

--- daily_download_US.py
+++ daily_download_US.py
@@ -14,13 +14,13 @@ NYSE_TICKER_LIST='http://www.nasdaq.com/screening/companies-by-industry.aspx?exc
 today=datetime.date.today()
 startdate=enddate=today-datetime.timedelta(1)
 
-def download_ticker(ticker,sd=startdate,ed=enddate):
+def download_ticker_US(ticker,sd=startdate,ed=enddate,market=''):
     fh=finance.fetch_historical_yahoo(ticker,sd,ed)
 
     #archive to disk
     #append to the existing files without the header
     #otherwise create it with the header
-    f='data/'+ticker+'.txt'
+    f='data/'+market+'/'+ticker+'.txt'
     if not os.path.exists(f):
         fhWrite=open(f,'w')
         header=fh.readline()
@@ -31,61 +31,42 @@ def download_ticker(ticker,sd=startdate,ed=enddate):
         fh.readline()
 
     r=fh.read()
-    r='\r\n'.join(sorted(r.split()))
-    fhWrite.write(r)
-    fhWrite.write('\r\n')
-    fhWrite.close()
+    if r:
+        #sort the records based on the dates
+        r='\r\n'.join(sorted(r.split()))
+        fhWrite.write(r)
+        #fhWrite.write('\r\n')
+        fhWrite.close()
 
-def download_tickers(tickers,sd=startdate,ed=enddate):
+def download_tickers_US(tickers,sd=startdate,ed=enddate,market=''):
     for ticker in tickers:
         #handle the error, if something happen to a stock, move on
         print(ticker)
         try:
-            download_ticker(ticker,sd,ed)
+            download_ticker_US(ticker,sd,ed,market)
         except (RuntimeError,ur.HTTPError):
             print('Error with ticker '+ticker)
 
 #for now focus only  in ASX
-def download_all_tickers(market='ASX',sd=startdate,ed=enddate):
-    if market=='ASX':
-        allTickers=str(ur.urlopen(ASX_TICKER_LIST).read()).strip("b")
+def download_all_tickers_US(market='ASX',sd=startdate,ed=enddate):
+    #new style of naming variable
+    amx_tickers = ur.urlopen(AMEX_TICKER_LIST).read().decode()
+    nasdaq_tickers = ur.urlopen(NASDAQ_TICKER_LIST).read().decode()
+    nyse_tickers = ur.urlopen(NYSE_TICKER_LIST).read().decode()
 
-        lines=allTickers.split("\\r\\n")
-        f='data/StockList/ASX.txt'
-        fhWrite=open(f,'w')
+    data=amx_tickers.split('\r\n')[1:]+nasdaq_tickers.split('\r\n')[1:]+nyse_tickers.split('\r\n')[1:]
+    all_tickers=[]
 
-        fhWrite.write('CompanyName,ASXCode,GICS\n')
-        for i in range(3,len(lines)):
-            fhWrite.write(lines[i]+"\n")
-        fhWrite.close()
+    f='data/StockList/US.txt'
+    fh_write=open(f,'w')
+    for line in data:
+        all_tickers.append(line.split(',')[0].strip('"').strip())
+        fh_write.write(line.split(',')[0].strip('"').strip()+'\n')
+    fh_write.close()
 
-        allTickers=mlab.csv2rec(f)
-        #print(allTickers)
+    download_tickers_US(all_tickers,sd,ed,market='US')
 
-        #get all the tickers
-        tickers=allTickers.asxcode;
-
-        tickers=[x+'.AX' for x in tickers]
-
-        download_tickers(tickers,sd,ed)
-        return tickers
-    elif market=='US':
-        #new style of naming variable
-        amx_tickers = ur.urlopen(AMEX_TICKER_LIST).read().decode()
-        nasdaq_tickers = ur.urlopen(NASDAQ_TICKER_LIST).read().decode()
-        nyse_tickers = ur.urlopen(NYSE_TICKER_LIST).read().decode()
-
-        data=amx_tickers.split('\r\n')[1:]+nasdaq_tickers.split('\r\n')[1:]+nyse_tickers.split('\r\n')[1:]
-        all_tickers=[]
-
-        f='data/StockList/US.txt'
-        fh_write=open(f,'w')
-        for line in data:
-            all_tickers.append(line.split(',')[0].strip('"').strip())
-            fh_write.write(line.split(',')[0].strip('"').strip()+'\n')
-        fh_write.close()
-
-        return all_tickers
+    return all_tickers
 
 
 #check the number of arguments
@@ -98,8 +79,10 @@ print(sys.argv)
 print(startdate)
 print(enddate)
 #ticker='1PG.AX'
-#sd=datetime.date(2014,1,1)
+sd=datetime.date(2014,1,1)
+ed=today
 #sd=datetime.date(2015,1,21)
 #ed=datetime.date(2016,3,23)
 #download_ticker(ticker,sd,ed)
-download_all_tickers('US',startdate,enddate)
+#download_all_tickers('US',startdate,enddate)
+download_all_tickers_US('US',sd,ed)
